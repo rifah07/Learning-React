22f50513f54d6c530741735defc2e202cea7e955
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-09-10 21:36:33 +0600

    css added

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,9 +1,13 @@
-function Welcome(){
-    return(
-        <div>
-            <h2>Welcome to Rifah's React App with Next JS</h2>
-        </div>
-    );
+import "./Welcome.css";
+
+function Welcome() {
+  return (
+    <div>
+      <h2 className="welcome-heading">
+        Welcome to Rifah's React App with Next JS
+      </h2>
+    </div>
+  );
 }
 
 export default Welcome;
